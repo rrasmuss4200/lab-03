cb5f4ae6decd30b4c7aa4fbe9daf4b8b87ef6ea4
rrasmuss07 <rrasmuss@ualberta.ca>
2026-01-21 17:00:51 -0700

    part 2

---
+++ app/src/main/java/com/example/listycity3/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.listycity3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener {
+    ListView cityList;
+    ArrayAdapter<City> cityAdapter;
+    ArrayList<City> dataList;
+
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] cities = { "Edmonton", "Vancouver", "Toronto" };
+        String[] provinces = { "AB", "BC", "ON" };
+
+        dataList = new ArrayList<City>();
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
+        cityList = findViewById(R.id.city_list);
+        cityAdapter = new cityArrayAdapter(this, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(v -> {
+            new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/listycity3/cityArrayAdapter.java
@@ -0,0 +1,42 @@
+package com.example.listycity3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.listycity3.City;
+
+import java.util.ArrayList;
+
+public class cityArrayAdapter extends ArrayAdapter<City> {
+
+    public cityArrayAdapter(Context context, ArrayList<City> cities) {
+        super(context, 0, cities);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.content,
+                    parent, false);
+        } else {
+            view = convertView;
+        }
+        City city = getItem(position);
+        TextView cityName = view.findViewById(R.id.city_text);
+        TextView provinceName = view.findViewById(R.id.province_text);
+        cityName.setText(city.getName());
+        provinceName.setText(city.getProvince());
+        return view;
+    }
+
+}
